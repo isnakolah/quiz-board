cb35f601a115c46d79705c108ed732fecb8ef85a
Daniel Nakolah <nakolahdaniel1@gmail.com>
2019-02-24 22:43:24 +0300

    add nav.js

---
+++ js/nav.js
@@ -0,0 +1,32 @@
+$('document').ready(function(){
+  var questionOne, questionTwo, questionThree, questionFour, questionFive, questionSix, questionSeven, questionEight, questionNine, questionTen; 
+  $('#1a').click(function(){
+    $('#question-two').show(215);
+    $('#1aa').addClass('clicked')
+    $('#1a').addClass('clicked')
+    $('#1bb').removeClass('clicked')
+    $('#1b').removeClass('clicked')
+    return
+  });
+  $('#1a').dblclick(function(){
+    $('#question-two').hide(215);
+    $('#1aa').removeClass('clicked')
+    $('#1a').removeClass('clicked')
+    questionOne = 0;
+  });
+  console.log(questionOne)
+  $('#1b').click(function(){
+    $('#question-two').show(215);
+    $('#1bb').addClass('clicked')
+    $('#1b').addClass('clicked')
+    $('#1aa').removeClass('clicked')
+    $('#1a').removeClass('clicked')
+    questionOne = 10;
+  });
+  $('#1b').dblclick(function(){
+    $('#question-two').hide(215);
+    $('#1bb').removeClass('clicked')
+    $('#1b').removeClass('clicked')
+    questionOne = 0;
+  });
+})
--- js/scripts.js
+++ js/scripts.js
@@ -5,38 +5,6 @@ $(document).ready(function(){
   });
   //Initializing the marking protocol.
   let totalMarks;
-  var questionOne, questionTwo, questionThree, questionFour, questionFive, questionSix, questionSeven, questionEight, questionNine, questionTen; 
-  //knowing that it has been clicked twice.
-  var clickedTwice = null;
-  $('#1a').click(function(){
-    $('#question-two').show(215);
-    $('#1aa').addClass('clicked')
-    $('#1a').addClass('clicked')
-    $('#1bb').removeClass('clicked')
-    $('#1b').removeClass('clicked')
-    return
-  });
-  $('#1a').dblclick(function(){
-    $('#question-two').hide(215);
-    $('#1aa').removeClass('clicked')
-    $('#1a').removeClass('clicked')
-    questionOne = 0;
-  });
-  console.log(questionOne)
-  $('#1b').click(function(){
-    $('#question-two').show(215);
-    $('#1bb').addClass('clicked')
-    $('#1b').addClass('clicked')
-    $('#1aa').removeClass('clicked')
-    $('#1a').removeClass('clicked')
-    questionOne = 10;
-  });
-  $('#1b').dblclick(function(){
-    $('#question-two').hide(215);
-    $('#1bb').removeClass('clicked')
-    $('#1b').removeClass('clicked')
-    questionOne = 0;
-  });
     $('#next1').click(function(){
       $('#question-2').show(350);
       $('#question-1').hide(10);
