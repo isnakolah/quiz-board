2a03960ee790ce2a10c3fd2f3d10609fcf59e521
Daniel Nakolah <nakolahdaniel1@gmail.com>
2019-02-24 19:30:49 +0300

    let it be easily detected when a user clicks twice

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,12 @@ $(document).ready(function(){
     $('#question-1').show(500);
     $('#intro').hide(10);
   });
-
+  //Initializing the marking protocol.
+  let totalMarks;
+  var questionOne, questionTwo, questionThree, questionFour, questionFive, questionSix, questionSeven, questionEight, questionNine, questionTen; 
+  //knowing that it has been clicked twice.
+  var clickedTwice = null;
+  $('#1a').click(function(){
+    
+  });
 });
