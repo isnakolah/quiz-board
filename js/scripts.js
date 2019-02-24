f6771f0595e97a2667ed67d9dcd1859a74a9f239
Daniel Nakolah <nakolahdaniel1@gmail.com>
2019-02-24 20:17:36 +0300

    add effect when clicked

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,14 @@ $(document).ready(function(){
   //knowing that it has been clicked twice.
   var clickedTwice = null;
   $('#1a').click(function(){
-    
+    $('#question-two').show(215);
+    $('#1aa').css('background','brown');
+    $('#1aa').css('background','brown');
+    questionOne = 10;
   });
+  $('#1a').dblclick(function(){
+    $('#question-two').hide(215);
+    questionOne = 0;
+  });
+  console.log(questionOne)
 });
