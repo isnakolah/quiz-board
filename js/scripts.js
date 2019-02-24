b6457a488022fea3082c304b67fc1e6af9d7fac9
Daniel Nakolah <nakolahdaniel1@gmail.com>
2019-02-24 21:50:37 +0300

    add questtion two

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,9 @@ $(document).ready(function(){
     $('#question-two').show(215);
     $('#1aa').addClass('clicked')
     $('#1a').addClass('clicked')
-    questionOne = 10;
+    $('#1bb').removeClass('clicked')
+    $('#1b').removeClass('clicked')
+    return
   });
   $('#1a').dblclick(function(){
     $('#question-two').hide(215);
@@ -21,5 +23,22 @@ $(document).ready(function(){
     questionOne = 0;
   });
   console.log(questionOne)
-  
+  $('#1b').click(function(){
+    $('#question-two').show(215);
+    $('#1bb').addClass('clicked')
+    $('#1b').addClass('clicked')
+    $('#1aa').removeClass('clicked')
+    $('#1a').removeClass('clicked')
+    questionOne = 10;
+  });
+  $('#1b').dblclick(function(){
+    $('#question-two').hide(215);
+    $('#1bb').removeClass('clicked')
+    $('#1b').removeClass('clicked')
+    questionOne = 0;
+  });
+    $('#next1').click(function(){
+      $('#question-2').show(500);
+      $('#question-1').hide(10);
+    })
 });
