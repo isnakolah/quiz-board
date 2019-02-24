9c5711664e276bf1c511ef96e068d7bcbf45e449
Daniel Nakolah <nakolahdaniel1@gmail.com>
2019-02-24 22:31:23 +0300

    use cdns

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $('#begin').click(function(){
-    $('#question-1').show(500);
+    $('#question-1').show(350);
     $('#intro').hide(10);
   });
   //Initializing the marking protocol.
@@ -38,11 +38,15 @@ $(document).ready(function(){
     questionOne = 0;
   });
     $('#next1').click(function(){
-      $('#question-2').show(500);
+      $('#question-2').show(350);
       $('#question-1').hide(10);
     })
     $('#next2').click(function(){
-      $('#question-3').show(500);
+      $('#question-3').show(350);
       $('#question-2').hide(10);
+    })
+    $('#next3').click(function(){
+      $('#question-4').show(350);
+      $('#question-3').hide(10);
     })
 });
